3156aaeee3abe4c012018f57f9635fd718a91f04
ThinkPowerXamarin <lydia.shih@thinkpower.com.tw>
2015-01-13 16:33:45 +0800

    Demo

---
+++ WebServiceDemo.Droid/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using WebServiceDemo.Service;
+
+namespace WebServiceDemo.Droid
+{
+    [Activity(Label = "WebServiceDemo.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected  override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            MyService service = new MyService();
+            button.Click +=async (sender,e)=>
+            {
+                var result =await service.GetData(999);
+                Toast.MakeText(this, result, ToastLength.Long).Show();
+            };
+
+           
+        }
+    }
+}
+
